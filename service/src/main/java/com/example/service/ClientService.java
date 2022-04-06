b75d81194a533ed613083126dcfb89eacc52df31
Daniel Capla <capla.daniel@gmail.com>
2022-04-06 23:57:16 +0200

    Add ClientMapper, Update ClientService and ClientRepository

--- service/src/main/java/com/example/service/ClientService.java
+++ service/src/main/java/com/example/service/ClientService.java
@@ -1,11 +1,38 @@
 package com.example.service;
 
+import com.example.db.entity.Client;
+import com.example.db.repository.ClientRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ClientService {
 
+    private ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
 
+    public ResponseEntity getAllClients() {
+
+        List<Client> clientList = clientRepository.getAll();
+        List<Client> clientListWithoutContractDetails = new ArrayList<>();
+
+        for (Client client :
+                clientList) {
+
+            //TODO:Create mapper, create DTOs, map entities do DTOs
+
+            //clientListWithoutContractDetails.add()
+
+        }
+
+        return ResponseEntity.status(200).build();
+    }
 
 }
---
+++ service/src/main/java/com/example/service/mapper/ClientMapper.java
@@ -0,0 +1,5 @@
+package com.example.service.mapper;
+
+
+public class ClientMapper {
+}
