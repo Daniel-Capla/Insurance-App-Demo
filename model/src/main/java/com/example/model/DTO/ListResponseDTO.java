1bfbb4ba0961adf8f68b1db07d5e0924521d0865
Daniel Capla <capla.daniel@gmail.com>
2022-04-07 00:37:26 +0200

    Add mapping to DTO, Edit pom dependencies and ListResponseDTO variables

--- model/src/main/java/com/example/model/DTO/ListResponseDTO.java
+++ model/src/main/java/com/example/model/DTO/ListResponseDTO.java
@@ -1,4 +1,18 @@
 package com.example.model.DTO;
 
+import com.example.db.entity.MailingAddress;
+import com.example.db.entity.OfficialAddress;
+import lombok.Data;
+
+
+@Data
 public class ListResponseDTO {
+
+    private String name;
+    private String surname;
+    private String dateOfBirth;
+    private OfficialAddress officialAddress;
+    private MailingAddress mailingAddress;
+
+
 }
--- service/src/main/java/com/example/service/mapper/ClientMapper.java
+++ service/src/main/java/com/example/service/mapper/ClientMapper.java
@@ -1,5 +1,13 @@
 package com.example.service.mapper;
 
+import com.example.db.entity.Client;
+import com.example.model.DTO.ListResponseDTO;
+import org.mapstruct.Mapper;
 
-public class ClientMapper {
+@Mapper
+public interface ClientMapper {
+
+    ListResponseDTO listResponseDTO(Client client);
+
+
 }
